301c51049205557de0dbb35a22dfe5c6db9be90d
gurungDev <ngrgurung890@gmail.com>
2025-04-22 12:38:02 +0545

    order items add fix

--- controller/orderItem.Controller.js
+++ controller/orderItem.Controller.js
@@ -115,7 +115,7 @@ class OrderItemsController {
           message: "successfully deleted",
         });
       }
-      orderItem.price = orderItem.menuID.price * orderItem.quantity;
+      orderItem.price = orderItem.menuID.price ;
       const updatedOrderItem = await orderItem.save();
       return ExpressResponse.success(res, { data: updatedOrderItem });
     } catch (error) {
